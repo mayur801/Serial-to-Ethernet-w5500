87f0484f712d81fbe45e1a37820f42e4da9a5d8d
mayur801 <78903681+mayur801@users.noreply.github.com>
2025-06-23 17:54:50 +0530

    Add files via upload
    libs

---
+++ w5500/w5500_spi.h
@@ -0,0 +1,13 @@
+/*
+ * w5500_spi.h
+ *
+ *  Created on: Sep 8, 2023
+ *      Author: Mayur Jagadale
+ */
+
+#ifndef SRC_W5500_W5500_SPI_H_
+#define SRC_W5500_W5500_SPI_H_
+
+void W5500Init(void);
+
+#endif /* SRC_W5500_W5500_SPI_H_ */
